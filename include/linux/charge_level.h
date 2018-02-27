c7187818d69a7fe59858e8af75123b23173b12f3
kozmo21 <kramer801@yahoo.com>
2018-02-26 23:31:33 -0600

    Update charge level

--- include/linux/charge_level.h
+++ include/linux/charge_level.h
@@ -10,7 +10,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * (values modified by psndna88@xda for NOTE II)
  */
 
 extern int ac_level;
@@ -20,29 +19,26 @@ extern int wireless_level;
 extern int ignore_unstable_power;
 extern int ignore_safety_margin;
 
-/* Modified Values for I930x and Note II compatibility  psndna88@xda */
-
-#if defined CONFIG_MACH_M0 || defined CONFIG_MACH_M3
-#define AC_CHARGE_LEVEL_DEFAULT 1000 	/* MACH_M0 & MACH_M3 */
-#define AC_CHARGE_LEVEL_MAX 1500	/* MACH_M0 & MACH_M3 */
+#ifdef CONFIG_MACH_T0
+#define AC_CHARGE_LEVEL_DEFAULT 1700
 #else
-#define AC_CHARGE_LEVEL_DEFAULT 1800 	/* MACH_T0 */
-#define AC_CHARGE_LEVEL_MAX 2000 	/* MACH_T0 */
+#define AC_CHARGE_LEVEL_DEFAULT 1800
 #endif
-#define AC_CHARGE_LEVEL_MIN 100
+#define AC_CHARGE_LEVEL_MIN 0
 
-#define USB_CHARGE_LEVEL_DEFAULT 475
+#ifdef CONFIG_MACH_T0
+#define AC_CHARGE_LEVEL_MAX 2000
+#else
+#define AC_CHARGE_LEVEL_MAX 1500
+#endif
+
+#define USB_CHARGE_LEVEL_DEFAULT 500
 #define USB_CHARGE_LEVEL_MIN 0
 #define USB_CHARGE_LEVEL_MAX 1000
 
-#if defined CONFIG_MACH_M0 || defined CONFIG_MACH_M3
-#define WIRELESS_CHARGE_LEVEL_DEFAULT 475	 /* MACH_M0 & MACH_M3 */
-#define WIRELESS_CHARGE_LEVEL_MAX 1000		 /* MACH_M0 & MACH_M3 */
-#else
-#define WIRELESS_CHARGE_LEVEL_DEFAULT 475 	/* MACH_T0 */
-#define WIRELESS_CHARGE_LEVEL_MAX 1900 		/* MACH_T0 */
-#endif
-#define WIRELESS_CHARGE_LEVEL_MIN 100
+#define WIRELESS_CHARGE_LEVEL_DEFAULT 500
+#define WIRELESS_CHARGE_LEVEL_MIN 0
+#define WIRELESS_CHARGE_LEVEL_MAX 1000
 
 #define IGNORE_UNSTABLE_POWER_DEFAULT 0
 #define IGNORE_SAFETY_MARGIN_DEFAULT 0
